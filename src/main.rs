1cf80938758acf24346cd26b817549a64acdd11c
nilpe <knohitokasou@gmail.com>
2024-11-26 17:27:08 +0900

    Cargo.lockとmain.rsを更新し、新しい依存関係を追加し、画像生成ロジックを改善しました

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
-use image::{RgbImage, Rgb};
 use clap::Parser;
-use std::path::PathBuf;
+use image::{Rgb, RgbImage};
+use std::{error::Error, io, path::PathBuf};
 #[derive(Parser, Debug)]
 #[command(name = "bytevisualizer")]
 #[command(about = "Visualize byte data")]
 struct Args {
-    #[clap(short, long, parse(from_os_str))]
+    #[clap(short, long)]
     input: PathBuf,
-    #[clap(short='o', long="output", parse(from_os_str))]
+    #[clap(short, long)]
     output: PathBuf,
 }
 
 fn main() {
-
     let args = Args::parse();
 
     println!("入力ファイル: {:?}", args.input);
@@ -23,29 +22,33 @@ fn main() {
         eprintln!("入力ファイルが存在しません: {:?}", args.input);
         std::process::exit(1);
     }
-    let d=    load_file_to_u8_array(&args.input).unwrap();
     let width = 1024;
-    let height = 256;
+
+    let mut  d = load_file_to_u8_array(&args.input).unwrap();
+    let paddinglen = width - d.len() % width;
+    for _ in 0..paddinglen {
+        d.push(0);
+    }
+    let height = d.len() / width;
 
     // 新しい画像を作成
-    let mut img = RgbImage::new(width, height);
+    let mut img = RgbImage::new(width as u32, height as u32);
 
     for x in 0..width {
         for y in 0..height {
             // グラデーションを生成
-            img.put_pixel(x, y, Rgb([x as u8, y as u8, 128]));
+            img.put_pixel(x as u32, y as u32, Rgb([d[y * width + x], 0, 0]));
         }
     }
 
     // ファイルに保存
     img.save("output.png").unwrap();
 }
-fn load_file_to_u8_array(file_path: &PathBuf) -> Result<Vec<u8>> {
+fn load_file_to_u8_array(file_path: &PathBuf) -> Result<Vec<u8>, io::Error> {
     use std::fs::File;
     use std::io::Read;
-
-    let mut file = File::open(file_path).ok()?;
-    let mut buffer = Vec::new();
-    file.read_to_end(&mut buffer).ok()?;
-    Some(buffer)
+    let mut file = File::open(file_path)?;
+    let mut buf = Vec::new();
+    file.read_to_end(&mut buf)?;
+    Ok(buf)
 }
